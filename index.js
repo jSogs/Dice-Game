06add342c065b6522daebc6f4d9bf7c0cb79e7d4
jSogs <121254364+jSogs@users.noreply.github.com>
2023-03-15 19:16:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = 1 + Math.floor(Math.random()*6);
 var imgNo1= "images/dice"+randomNumber1+".png";
 var randomNumber2 = 1 + Math.floor(Math.random()*6);
-var imgNo2 = "images/dice"+randomNumber2+".png";
+var imgNo2 = "dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src",imgNo1);
 document.querySelector(".img2").setAttribute("src",imgNo2);
 document.querySelector("h1").textContent= (imgNo1 > imgNo2) ? "🚩Player 1 Wins!" : (imgNo1 === imgNo2) ? "Draw!":"Player 2 Wins! 🚩"
